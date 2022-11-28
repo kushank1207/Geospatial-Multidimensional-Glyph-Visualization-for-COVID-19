ca7e1e18a4c74af5d65c28581c0ce799da079132
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2022-11-27 21:20:48 -0500

    Added task_3

---
+++ core/task_3.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import plotly.express as px
+
+def fid_task_3(country, continent, COVID_df):
+    if country:
+        data = COVID_df[COVID_df['location'].isin(country)]
+        land_data = country
+    else:
+        data = COVID_df[COVID_df['continent'].isin(continent)]
+        land_data = continent
+
+    recent_tests_data_frame = pd.DataFrame(columns=['location', 'total_tests', 'date'])
+
+    color_list = px.colors.qualitative.Alphabet + px.colors.qualitative.Dark24 + px.colors.qualitative.Dark2
+    color_dict = {land_data[index]: color_list[index]   for index in range(len(land_data))}
+
+    for country in data:
+        country_recent_data = COVID_df.loc[(COVID_df['location'] == country)
+                                                 & pd.notnull(covid19_data_frame['total_tests']),
+                                                 ['location', 'total_tests', 'date']]
+    if not country_recent_data.empty:
+        recent_tests_data_frame = pd.concat([recent_tests_data_frame, country_recent_data.iloc[[-1]]])
+
+    fig3 = px.pie(recent_tests_data_frame, values='total_tests', names='location', title='Pie Chart'
+              , color='location', color_discrete_map=color_dict, hover_data=['date']
+              , labels={'location': 'European country', 'date': 'Recent data available date',
+                        'total_tests': 'Total tests'}, height=700)
+
+    fig3.update_traces(textposition='inside', textinfo='percent+label'
+                   , hovertemplate='Total tests: %{value} <br>Recent data available date,' +
+                                   'European country: %{customdata}</br>')
